38c7dfc43d49b28f3caacc89aada601bd9a8d47c
Hector Trujillo <hectort88@outlook.com>
2024-02-02 18:18:04 -0500

    Add graphics

--- SFMLGame/Main.cpp
+++ SFMLGame/Main.cpp
@@ -1,36 +1,43 @@
 #include <SFML/Graphics.hpp>
 
+sf::Vector2f viewSize(1024, 768);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "Hello SFML!!!", sf::Style::Default);
+
+sf::Sprite skySprite;
+sf::Texture skyTexture;
+sf::Sprite bgSprite;
+sf::Texture bgTexture;
+sf::Texture heroTexture;
+sf::Sprite heroSprite;
+
+void draw()
+{
+	window.draw(skySprite);
+	window.draw(bgSprite);
+	window.draw(heroSprite);
+}
+
+void init()
+{
+	skyTexture.loadFromFile("Assets/graphics/sky.png");
+	skySprite.setTexture(skyTexture);
+	bgTexture.loadFromFile("Assets/graphics/bg.png");
+	bgSprite.setTexture(bgTexture);
+	heroTexture.loadFromFile("Assets/graphics/hero.png");
+	heroSprite.setTexture(heroTexture);
+	heroSprite.setPosition(sf::Vector2f(viewSize.x / 2, viewSize.y / 2));
+	heroSprite.setOrigin(heroTexture.getSize().x / 2, heroTexture.getSize().y / 2);
+}
+
 int main()
 {
-	sf::Vector2f viewSize(1024, 768);
-	sf::VideoMode vm(viewSize.x, viewSize.y);
-	sf::RenderWindow window(vm, "Hello SFML!!!", sf::Style::Default);
-
-	// Square
-	sf::RectangleShape rectangle(sf::Vector2f(500.0f, 300.0f));
-	rectangle.setFillColor(sf::Color::Blue);
-	rectangle.setPosition(viewSize.x / 2, viewSize.y / 2);
-	rectangle.setOrigin(sf::Vector2f(rectangle.getSize().x / 2, rectangle.getSize().y / 2));
-
-	// Circle
-	sf::CircleShape circle(100);
-	circle.setFillColor(sf::Color::Red);
-	circle.setPosition(viewSize.x / 2, viewSize.y / 2);
-	circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
-
-	// Triangle
-	sf::ConvexShape triangle;
-	triangle.setPointCount(3);
-	triangle.setPoint(0, sf::Vector2f(-100, 0));
-	triangle.setPoint(1, sf::Vector2f(0, -100));
-	triangle.setPoint(2, sf::Vector2f(100, 0));
-	triangle.setFillColor(sf::Color::Cyan);
-	triangle.setPosition(viewSize.x / 2, viewSize.y / 2);
-
+	init();
+	sf::Event event;
 	
 	while (window.isOpen())
 	{
-		sf::Event event;
+		
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
@@ -38,9 +45,7 @@ int main()
 		}
 
 		window.clear(sf::Color::Black);
-		window.draw(rectangle);
-		window.draw(circle);
-		window.draw(triangle);
+		draw();
 		window.display();
 	}
 	return 0;
