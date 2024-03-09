9cd666fc8ca4539eaa779565e8a7fea1d0dde560
Hector Trujillo <hectort88@outlook.com>
2024-03-09 18:03:27 -0500

    Sound and text

--- SFMLGame/Main.cpp
+++ SFMLGame/Main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include "Hero.hpp"
 #include "Enemy.hpp"
@@ -19,6 +20,40 @@ std::vector<Enemy*> enemies;
 std::vector<Rocket*> rockets;
 float currentTime;
 float prevTime = 0.0f;
+int score = 0;
+bool gameOver = true;
+
+//fonts
+sf::Font headingFont;
+sf::Text headingText;
+sf::Font scoreFont;
+sf::Text scoreText;
+
+//music
+sf::Music bgMusic;
+sf::SoundBuffer fireBuffer;
+sf::SoundBuffer hitBuffer;
+sf::Sound fireSound(fireBuffer);
+sf::Sound hitSound(hitBuffer);
+
+void reset()
+{
+	score = 0;
+	currentTime = 0.0f;
+	prevTime = 0.0;
+
+	for (auto *enemy : enemies)
+	{
+		delete(enemy);
+	}
+	for (auto* rocket : rockets)
+	{
+		delete(rocket);
+	}
+
+	enemies.clear();
+	rockets.clear();
+}
 
 static void spawnEnemy()
 {
@@ -56,6 +91,7 @@ static void shoot()
 	position.y += 16;
 	position.x += 40;
 	rocket->init("Assets/graphics/rocket.png", position, 600.0f);
+	fireSound.play();
 	rockets.push_back(rocket);
 }
 
@@ -72,6 +108,8 @@ static void draw()
 	window.draw(skySprite);
 	window.draw(bgSprite);	
 	window.draw(hero.getSprite());
+	window.draw(headingText);
+	window.draw(scoreText);
 
 	for (Enemy *enemy: enemies) {
 		window.draw(enemy->getSprite());
@@ -88,7 +126,31 @@ static void init()
 	skySprite.setTexture(skyTexture);
 	bgTexture.loadFromFile("Assets/graphics/bg.jpg");
 	bgSprite.setTexture(bgTexture);
+	
 	hero.init("Assets/graphics/hero3.png", sf::Vector2f(viewSize.x * 0.25f, viewSize.y * 0.5f), 200);
+	headingFont.loadFromFile("Assets/fonts/Aligator.otf");
+	headingText.setFont(headingFont);
+	headingText.setString("Kill them all");
+	headingText.setCharacterSize(84);
+	headingText.setFillColor(sf::Color::Red);
+	sf::FloatRect headingbounds = headingText.getLocalBounds();
+	headingText.setOrigin(headingbounds.width / 2, headingbounds.height / 2);
+	headingText.setPosition(sf::Vector2f(viewSize.x * 0.5f, viewSize.y * 0.10f));
+
+	scoreFont.loadFromFile("Assets/fonts/arial.ttf");
+	scoreText.setFont(scoreFont);
+	scoreText.setString("Score: 0");
+	scoreText.setCharacterSize(45);
+	scoreText.setFillColor(sf::Color::White);
+	sf::FloatRect scorebounds = scoreText.getLocalBounds();
+	scoreText.setOrigin(scorebounds.width / 2, scorebounds.height / 2);
+	scoreText.setPosition(sf::Vector2f(viewSize.x * 0.5f, viewSize.y * 0.20f));
+
+	// bgMusic.openFromFile("Assets/audio/bgMusic.ogg");	
+	// bgMusic.play();
+	hitBuffer.loadFromFile("Assets/audio/hit.ogg");
+	fireBuffer.loadFromFile("Assets/audio/fire.ogg");
+
 	srand((int)time(0));
 }
 
@@ -111,7 +173,16 @@ static void update(float dt)
 
 			if (checkCollision(rocket->getSprite(), enemy->getSprite()))
 			{
-				std::cout << "Rocket " << idx_rocket << " killed enemy " << idx_enemy << "\n";
+				score++;
+				hitSound.play();
+				std::cout <<" killed enemies: " << score << "\n";
+
+				std::string finalScore = "Kills: " + std::to_string(score);
+				scoreText.setString(finalScore);
+				sf::FloatRect scorebounds = scoreText.getLocalBounds();
+				scoreText.setOrigin(scorebounds.width / 2, scorebounds.height / 2);
+				scoreText.setPosition(sf::Vector2f(viewSize.x * 0.5f, viewSize.y * 0.20f));
+
 				rockets.erase(rockets.begin() + idx_rocket);
 				enemies.erase(enemies.begin() + idx_enemy);
 
@@ -130,6 +201,7 @@ static void update(float dt)
 		{
 			enemies.erase(enemies.begin() + i);
 			delete(enemy);
+			gameOver = true;
 		}
 	}
 
@@ -139,9 +211,9 @@ static void update(float dt)
 		Rocket* rocket = rockets[i];
 		rocket->update(dt);
 		if (rocket->getSprite().getPosition().x > viewSize.x)
-		{
-			rockets.erase(rockets.begin() + i);
-			delete(rocket);
+		{			
+			rockets.erase(rockets.begin() + i);			
+			delete(rocket);			
 		}
 	}
 	
@@ -157,7 +229,7 @@ static void updateInput()
 				hero.jump(750.0f);
 			}
 			if (event.key.code == sf::Keyboard::Space) {
-				shoot();
+				shoot();			
 			}
 		}
 		if (event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed)
